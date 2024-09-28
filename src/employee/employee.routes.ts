0442aea9c053f80b42648d7109f3abe7f64a1d03
martularrauri <martibelenlarrauri@gmail.com>
2024-09-28 18:41:38 -0300

    SanitizeEmploye

--- src/employee/employee.routes.ts
+++ src/employee/employee.routes.ts
@@ -1,10 +1,10 @@
 import { Router } from "express"
-import { findAll, findOne, add, update, remove } from "./employee.controler.js"
+import {sanitizeEmployeeInput ,findAll, findOne, add, update, remove } from "./employee.controler.js"
 
 export const employeeRouter = Router()
 
 employeeRouter.get('/', findAll)
 employeeRouter.get('/:id', findOne)
-employeeRouter.post('/', add)
-employeeRouter.put('/:id', update)
+employeeRouter.post('/', sanitizeEmployeeInput, add)
+employeeRouter.put('/:id', sanitizeEmployeeInput, update)
 employeeRouter.delete('/:id', remove)
